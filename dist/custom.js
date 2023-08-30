1f989abd93a614391288ccf5aa34982199e37cd1
tuan-anh1605 <142969733+tuan-anh1605@users.noreply.github.com>
2023-08-30 21:35:24 +0700

    Add files via upload

---
+++ dist/custom.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	nav:true,
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:10000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+
+	  })
+$('#dichvu-chayngang').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:4,	// xuất hiện 1 ảnh thôi
+	margin:30,
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:10000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+
+	  })
+$('#tes').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:2,	// xuất hiện 1 ảnh thôi
+	margin:30,
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:10000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+
+	  })
